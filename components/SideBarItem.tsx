e5613f5f832723f3d3d98472ea313526be952800
David Grammatico <david.lugdunum@gmail.com>
2023-06-12 16:39:16 +0200

    update sidebar

--- components/SideBarItem.tsx
+++ components/SideBarItem.tsx
@@ -1,5 +1,31 @@
+import Link from 'next/link'
 import React from 'react'
+import { IconType } from 'react-icons'
+import { twMerge } from 'tailwind-merge'
 
-export default function SideBarItem() {
-  return <div>SideBarItem</div>
+type SideBarItemProps = {
+  icon: IconType
+  label: string
+  active: boolean
+  href: string
+}
+
+export default function SideBarItem({
+  icon: Icon,
+  label,
+  active,
+  href,
+}: SideBarItemProps) {
+  return (
+    <Link
+      className={twMerge(
+        `flex flex-row h-auto items-center w-full gap-x-4 text-sm font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1`,
+        active && `text-white`
+      )}
+      href={href}
+    >
+      <Icon size={20} />
+      <p className="truncate w-full">{label}</p>
+    </Link>
+  )
 }
